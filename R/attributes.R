976f616127aa4d73884d8cebb5b4d9c54a1d6ed6
Richard White <rjw26@cam.ac.uk>
2020-06-08 15:58:18 +0100

    Create get_gene_metadata and subset_to_samples functions

--- R/attributes.R
+++ R/attributes.R
@@ -5,7 +5,7 @@
 #'
 #' @param data df rna-seq data to get counts from
 #' @param samples df a samples df to subset the data to
-#' @param normalised logical indicatig whether to return
+#' @param normalised logical indicating whether to return
 #' normalised counts or raw
 #'
 #' @return data.frame
@@ -30,8 +30,55 @@ get_counts <- function(data, samples = NULL, normalised = FALSE) {
   # TO ADD: check all samples exist in counts
   # and if any are in counts that aren't in samples
   if (!is.null(samples)) {
-    count_data <- count_data[, samples$sample ]
+    count_data <- select(count_data, !!as.character(samples$sample))
   }
 
   return(count_data)
+}
+
+#' Get non count data for an RNA-seq dataset
+#'
+#' \code{get_gene_metadata} returns a data.frame of metadata,
+#' associated with the genes of an RNA-seq dataset
+#'
+#' @param data df rna-seq data to get gene metadata from
+#'
+#' @return data.frame
+#'
+#' @examples
+#' non_count_data <- get_gene_metadata(data)
+#'
+#' @export
+get_gene_metadata <- function(data) {
+  non_count_data <- data[ , !grepl("count", colnames(data)) ]
+  return(non_count_data)
+}
+
+#' Subset an RNA-seq dataset using a samples file
+#'
+#' \code{subset_to_samples} returns a data.frame of metadata,
+#' associated with the genes of an RNA-seq dataset
+#'
+#' @param data df rna-seq data to get counts from
+#' @param samples df a samples df to subset the data to
+#'
+#' @return data.frame
+#'
+#' @examples
+#' non_count_data <- subset_to_samples(data, samples)
+#'
+#' @export
+subset_to_samples <- function(data, samples) {
+  subset_metadata <- get_gene_metadata(data)
+  # get counts
+  counts <- get_counts(data, samples, normalised = FALSE)
+  # add back 'count' to end of column names
+  colnames(counts) <- paste(colnames(counts), 'count')
+
+  # get normalised counts and add normalised count back to col names
+  norm_counts <- get_counts(data, samples, normalised = TRUE)
+  colnames(norm_counts) <- paste(colnames(norm_counts), 'normalised count')
+
+  subset_data <- as_tibble(cbind(subset_metadata, counts, norm_counts))
+  return(subset_data)
 }
