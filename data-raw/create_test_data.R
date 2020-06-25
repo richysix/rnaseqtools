fbb7f8424dde2d7e34f8346f4b43f14f42d24b95
Richard White <rjw26@cam.ac.uk>
2020-06-25 11:23:39 +0100

    Move creating test data to separate script Change tests to fit with changes to functions
    New files:
    data-raw/create_test_data.R
    tests/testthat/test-load.R
    tests/testthat/test_data.rda
    tests/testthat/test_data.tsv
    tests/testthat/test_samples.tsv

---
+++ data-raw/create_test_data.R
@@ -0,0 +1,59 @@
+library(tibble)
+library(rprojroot)
+
+root_path <- find_root(is_rstudio_project)
+
+# setup test data file to load
+num_rows <- 100
+set.seed(208)
+starts <- sample(1:10000, num_rows)
+test_all_data <- tibble(
+  'chr' = sample(1:25, num_rows, replace = TRUE),
+  'start' = starts,
+  'end' = as.integer(starts + 100),
+  'strand' = sample(c('1', '-1'), num_rows, replace = TRUE),
+  'e95 Ensembl Gene ID' = paste0('ZFG', seq_len(num_rows)),
+  'Adjusted p value' = runif(num_rows),
+  'Gene name' = paste0('gene-', seq_len(num_rows))
+)
+test_all_data$chr <- factor(test_all_data$chr, levels = unique(test_all_data$chr))
+test_all_data$strand <- factor(test_all_data$strand)
+sample_num <- 6
+counts_list <- vector("list", length = sample_num)
+for(num in seq_len(sample_num)) {
+  sample_name <- paste0('sample-', num, ' count')
+  counts <- as.integer(floor(runif(num_rows)*100))
+  test_all_data[[sample_name]] <- counts
+  counts_list[[num]] <- counts
+}
+counts <- as.data.frame(do.call('cbind', counts_list))
+names(counts) <- paste0('sample-', seq_len(sample_num))
+
+norm_counts_list <- vector("list", length = sample_num)
+for(num in seq_len(sample_num)) {
+  sample_name <- paste0('sample-', num, ' normalised count')
+  norm_counts <- runif(num_rows)*100
+  test_all_data[[sample_name]] <- norm_counts
+  norm_counts_list[[num]] <- norm_counts
+}
+norm_counts <- as.data.frame(do.call('cbind', norm_counts_list))
+names(norm_counts) <- paste0('sample-', seq_len(sample_num))
+
+# write a file for testing load_rnaseq_data
+write.table(test_all_data, quote = FALSE,
+            row.names = FALSE, col.names = TRUE, sep = "\t",
+            file = file.path(root_path, "tests", "testthat", "test_data.tsv") )
+
+# create a samples test file
+samples_data <- tibble(
+  sample = factor(paste0('sample-', seq_len(sample_num))),
+  condition = factor(rep(c('wt', 'mut'), each = 3),
+                     levels = c('wt', 'mut')),
+  sex = rep(c('M', 'F'), 3)
+)
+# write a file for testing load_rnaseq_samples
+write_tsv(samples_data, path = file.path(root_path, "tests", "testthat", "test_samples.tsv") )
+
+# save test data as R objects
+save(test_all_data, counts, norm_counts, samples_data,
+     file = file.path(root_path, "tests", "testthat", "test_data.rda"))
--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -1,44 +1,7 @@
 context('load_data')
 library(rnaseqtools)
 
-# setup test data file to load
-num_rows <- 100
-set.seed(208)
-starts <- sample(1:10000, num_rows)
-test_all_data <- data.frame(
-  'chr' = sample(1:25, num_rows, replace = TRUE),
-  'start' = starts,
-  'end' = as.integer(starts + 100),
-  'strand' = sample(c('1', '-1'), num_rows, replace = TRUE),
-  'e95 Ensembl Gene ID' = paste0('ZFG', seq_len(num_rows)),
-  'Adjusted p value' = runif(num_rows),
-  'Gene name' = paste0('gene-', seq_len(num_rows))
-)
-test_all_data$chr <- factor(test_all_data$chr)
-sample_num <- 6
-counts_list <- vector("list", length = sample_num)
-for(num in seq_len(sample_num)) {
-  sample_name <- paste0('sample-', num, ' count')
-  counts <- as.integer(floor(runif(num_rows)*100))
-  test_all_data[[sample_name]] <- counts
-  counts_list[[num]] <- counts
-}
-counts <- as.data.frame(do.call('cbind', counts_list))
-names(counts) <- paste0('sample-', seq_len(sample_num))
-
-norm_counts_list <- vector("list", length = sample_num)
-for(num in seq_len(sample_num)) {
-  sample_name <- paste0('sample-', num, ' normalised count')
-  norm_counts <- runif(num_rows)*100
-  test_all_data[[sample_name]] <- norm_counts
-  norm_counts_list[[num]] <- norm_counts
-}
-norm_counts <- as.data.frame(do.call('cbind', norm_counts_list))
-names(norm_counts) <- paste0('sample-', seq_len(sample_num))
-
-names(test_all_data) <- gsub("\\.", " ", names(test_all_data))
-write.table(test_all_data, file = "test_data.tmp", quote = FALSE,
-            row.names = FALSE, col.names = TRUE, sep = "\t")
+load("test_data.rda")
 
 # named vector. names are colnames that should be altered
 # elements are what they should be changed to
@@ -60,31 +23,51 @@ test_that("standardise colnames",{
                  unname(colnames))
 })
 
-test_df <- data.frame(
-  'Chr' = as.integer(sample(seq_len(num_rows), num_rows)),
-  'Start' = as.numeric(sample(1:10000, num_rows)),
-  'End' = as.numeric(sample(1:10000, num_rows)),
-  'Strand' = as.integer(sample(c('1', '-1'), num_rows, replace = TRUE))
+expected_coltypes <- list(
+  chr = readr::col_factor(),
+  start = readr::col_integer(),
+  end = readr::col_integer(),
+  strand = readr::col_factor(),
+  `e95 Ensembl Gene ID` = readr::col_character(),
+  `Adjusted p value` = readr::col_double(),
+  `Gene name` = readr::col_character(),
+  `sample-1 count` = readr::col_integer(),
+  `sample-2 count` = readr::col_integer(),
+  `sample-3 count` = readr::col_integer(),
+  `sample-4 count` = readr::col_integer(),
+  `sample-5 count` = readr::col_integer(),
+  `sample-6 count` = readr::col_integer(),
+  `sample-1 normalised count` = readr::col_double(),
+  `sample-2 normalised count` = readr::col_double(),
+  `sample-3 normalised count` = readr::col_double(),
+  `sample-4 normalised count` = readr::col_double(),
+  `sample-5 normalised count` = readr::col_double(),
+  `sample-6 normalised count` = readr::col_double()
 )
-test_df <- standardise_coltypes(test_df)
+
 test_that("coltypes",{
-  expect_equal(class(test_df$Chr), 'factor')
-  expect_equal(class(test_df$Start), 'integer')
-  expect_equal(class(test_df$End), 'integer')
-  expect_equal(class(test_df$Strand), 'factor')
+  expect_equal(set_col_types('test_data.tsv'), expected_coltypes)
 })
 
-test_that("load RNAseq data",{
+test_that("colnames", {
   # colnames are currently not the same when tmp data is loaded back in
   # because colnames get changed
-  expect_true(any(names(test_all_data) != names(load_rnaseq_data('test_data.tmp'))))
-  # correct test_all_data names
-  names(test_all_data) <- c('Chr', 'Start', 'End', 'Strand', 'GeneID', 'adjp',
-                            'Name', paste0('sample-', seq_len(6), ' count'),
-                            paste0('sample-', seq_len(6), ' normalised count'))
+  expect_true(any(names(test_all_data) != names(load_rnaseq_data('test_data.tsv'))))
+})
+# correct test_all_data names
+names(test_all_data) <- c('Chr', 'Start', 'End', 'Strand', 'GeneID', 'adjp',
+                          'Name', paste0('sample-', seq_len(6), ' count'),
+                          paste0('sample-', seq_len(6), ' normalised count'))
 
-  expect_equal(load_rnaseq_data('test_data.tmp'),
-               test_all_data)
+test_that("load RNAseq data",{
+  expect_equal(as.data.frame(load_rnaseq_data('test_data.tsv')),
+               as.data.frame(test_all_data), check.attributes = FALSE)
+})
+
+# test loading samples file
+samples <- load_rnaseq_samples('test_samples.tsv')
+test_that("load samples", {
+  expect_equal(samples, samples_data)
 })
 
 test_that("get counts", {
